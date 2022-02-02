a0ea0a0ddb25435d47b3611e0e79bc5e143f07e2
byeung0x <94519768+byeung0x@users.noreply.github.com>
2022-02-02 14:13:32 +0800

    Update chart.js

--- chart.js
+++ chart.js
@@ -6,13 +6,7 @@ let series = {};
 
 
 fetch('https://query1.finance.yahoo.com/v7/finance/download/GOOGL?period1=1000073600&period2=1999960000&interval=1d&events=history&includeAdjustedClose=true',
-    body: JSON.stringify(data), // must match 'Content-Type' header
-    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-    credentials: 'same-origin', // include, same-origin, *omit
-    headers: {
-      'user-agent': 'Mozilla/4.0 MDN Example',
-      'content-type': 'application/json'
-	    )
+    mode: 'no-cors')
 	.then(function (response) {
 		return response.text();
 	})
